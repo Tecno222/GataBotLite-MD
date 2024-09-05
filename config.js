950a546e50a6980d7f3304f91358bc8c1dbad6fa
Tecno222 <ditecno57@gmail.com>
2024-09-05 18:43:18 -0300

    Update config.js Gu
    Gatabot para grupo de WhatsApp

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ global.isBaileysFail = false
 // ❰❰ methodCode ❱❱
 // [ES] > Agregue el número del Bot en "botNumberCode" si desea recibir código de 8 dígitos sin registrar el número en la consola.
 // [EN] > Add the Bot number in "botNumberCode" if you want to receive 8-digit code without registering the number in the console.
-global.botNumberCode = "" //example: "+59309090909"
+global.botNumberCode = "+5493872184632" //example: "+59309090909"
 global.confirmCode = "" // No tocar esto : Do not touch this line
 
 // ES ➜ Agregue el código de idioma el cual usará GataBot  
